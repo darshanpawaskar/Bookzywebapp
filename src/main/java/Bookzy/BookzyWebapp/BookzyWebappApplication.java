1ec96bc433cad34c854ef72d6819d97195dc644e
student <darshanpawaskar@gmail.com>
2021-08-02 00:34:35 +0000

    First commit

---
+++ src/main/java/Bookzy/BookzyWebapp/BookzyWebappApplication.java
@@ -0,0 +1,13 @@
+package Bookzy.BookzyWebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookzyWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookzyWebappApplication.class, args);
+	}
+
+}
